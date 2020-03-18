c15d04866ca353479e5374fca379c3f09e293b6e
shallinta <740159135@qq.com>
2020-03-18 20:04:50 +0800

    chore(build): build change

--- dist/z-progress-loading.common.js
+++ dist/z-progress-loading.common.js
@@ -561,7 +561,7 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"768e9e92-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/index.vue?vue&type=template&id=733e6d86&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"0d1a56d3-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/index.vue?vue&type=template&id=733e6d86&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{attrs:{"id":"z-progress-loading"}},[_c('canvas',{attrs:{"id":"z-progress-loading-canvas","width":_vm.width,"height":_vm.height}},[_c('i',[_vm._v("Not supported.")])])])}
 var staticRenderFns = []
 
